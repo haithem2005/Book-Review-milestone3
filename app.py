a01a5e2d62851ad932c146686f58b3c81c5356ac
haithem2005 <h.elhessawy@gmail.com>
2020-04-21 11:31:19 +0000

    initial commit

---
+++ app.py
@@ -0,0 +1,15 @@
+from flask import Flask
+import os
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello():
+    return "hello world"
+
+
+if __name__ == '__main__':
+    app.run(host=os.environ.get('IP'),
+            port=int(os.environ.get('PORT')),
+            debug=True)
