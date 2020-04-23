cdc7fe8212cc2c790e3cb3af9bc88f6a182674cd
haithem2005 <h.elhessawy@gmail.com>
2020-04-23 08:54:02 +0000

    adding base,books,reviews.html pages, also add their routes and functions in app.py

--- app.py
+++ app.py
@@ -1,12 +1,31 @@
-from flask import Flask
 import os
+from flask import Flask, render_template, request, redirect, url_for
+from flask_pymongo import PyMongo
+from bson.objectid import ObjectId
+from os import path
+if path.exists("env.py"):
+    import env
 
+# add confiqurations
 app = Flask(__name__)
+app.config["MONGO_DBNAME"] = 'book_review'
+app.config["MONGO_URI"] = os.environ.get('MONGO_URI')
+mongo = PyMongo(app)
 
 
 @app.route('/')
-def hello():
-    return "hello world"
+@app.route('/get_books')
+def get_books():
+    return render_template("books.html", books=list(mongo.db.books.find()),
+                           categories=mongo.db.categories.find())
+
+
+@app.route('/reviews/<book_id>')
+def reviews(book_id):
+    the_book = mongo.db.books.find_one({"_id": ObjectId(book_id)})
+    return render_template('reviews.html',
+                           book=the_book,
+                           reviews=mongo.db.reviews.find())
 
 
 if __name__ == '__main__':
